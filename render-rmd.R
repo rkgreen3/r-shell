2dee1066f04c69778afb5ff3670912c682653839
unknown <GLS-TAGR8305@AD.WISC.EDU>
2019-11-22 10:47:37 -0600

    send report to reports folder and select by plot 1

--- render-rmd.R
+++ render-rmd.R
@@ -2,4 +2,6 @@ require(rmarkdown)
 require(knitr)
 require(tidyverse)
 
-render("surveys-report.Rmd")
+render("surveys-report.Rmd",
+       output_dir = "reports",
+       params = list(file = "data/plot1.csv"))
